d7f54396f2e17e888a46a379bf3c41c704947ee5
Mellisa2636 <142491255+Mellisa2636@users.noreply.github.com>
2023-09-29 16:06:17 +0300

    код 4 Лекции : Панель админки

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from .models import Advertisements
+from .models import Advertisements # импортируем модель из models текущей папки
 # Register your models here.
 
-class AdvertisementsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction']
+class AdvertisementsAdmin(admin.ModelAdmin): # наследуем класс ModelAsmin, потому что это админский файлик
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction'] # демонстрация модели, ее поля
     list_filter = ['price', 'auction']
-    actions = ['make_auction_false','make_auction_true']
-    fieldsets = (
+    actions = ['make_auction_false','make_auction_true'] # кнопочки-действия
+    fieldsets = (                     # смысловые блоки в добавлении обьявления
         ('Общее', {
             'fields': ('title', 'description')
         }),
         ('Финансы', {
             'fields': ('price', 'auction'),
-            'classes': ['collapse']
+            'classes': ['collapse', 'wide']   # collapse - возможность показать/скрыть; wide - на всю ширину
         })
     )
-    
+
     @admin.action(description='Убрать возможность торга')
     def make_auction_false(self, request, queryset):
         queryset.update(auction=False)
@@ -26,4 +26,4 @@ class AdvertisementsAdmin(admin.ModelAdmin):
 
 
 
-admin.site.register(Advertisements, AdvertisementsAdmin)
+admin.site.register(Advertisements, AdvertisementsAdmin) # регистрация модели
