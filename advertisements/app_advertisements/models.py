f6f794be909662658a589001328a6d09ad1f2fb1
Mellisa2636 <142491255+Mellisa2636@users.noreply.github.com>
2023-09-29 15:35:53 +0300

    код 4 лекции : Панель администрирования (ДЗ)

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -5,7 +5,7 @@ from django.utils.html import format_html
 # Create your models here.
 class Advertisements(models.Model):
     title = models.CharField(verbose_name='Заголовок',  max_length=128) # charhield - текстовое поле
-    description = models.TextField('Описание') #
+    description = models.TextField('Описание') # TextField - тестовое поле большего размера
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2) #digits - всего цифер; places - цифер после запятой
     auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
     created_at = models.DateTimeField(auto_now_add=True) # auto_now_ add - время добавления автоматически
@@ -18,3 +18,17 @@ class Advertisements(models.Model):
             return format_html('<span style="color: green; fond-weight: bold">Сегодня в {}</span>', created_at_time)
 
         return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+     
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date()==timezone.now().date():
+            updated_time=self.updated_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+
+    def __str__(self):
+            return f"Advertisement(id={self.id}, title={self.title}, price={self.price})" # магический метод str - возвращает текущий обьект
+
+    class Meta:
+        db_table = "advertisements" # класс, которы позволяет внести изменения в какой-то уже существующий класс
